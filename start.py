21f41b7078412c65436db6153a32c7517a21de97
Administrator <admin@example.com>
2019-09-23 22:32:35 +0800

    d

--- start.py
+++ start.py
@@ -33,11 +33,12 @@ else:
 
 tf.flags.DEFINE_string('test_type', 'A', 'test_type, default: A')
 
-tf.flags.DEFINE_integer('epoch', 10, 'test_type, default: 1000')
+
 tf.flags.DEFINE_float('real_label', 0.9, 'real_label, default: 0.9')
 
 
 if(model == 'CycleGAN'):
+    tf.flags.DEFINE_integer('epoch', 50, 'test_type, default: 1000')
     tf.flags.DEFINE_string('data_name', 'horse_zebra_256', 'test_type, default: apple_orange')
     tf.flags.DEFINE_integer('input_image_weight', 256, 'image size, default: 128')
     tf.flags.DEFINE_integer('input_image_height', 256, 'image size, default: 128')
