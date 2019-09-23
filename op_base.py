cd51400afbfaea9481ecbcef7c6c203e6a25707d
Administrator <admin@example.com>
2019-09-23 16:00:21 +0800

    dd

--- op_base.py
+++ op_base.py
@@ -27,8 +27,10 @@ class op_base(object):
         self.test_type = args.test_type
 
 
-        self.model_save_path = os.path.join('model',self.model)
+        self.model_save_path = os.path.join('model',self.model,self.data_name)
         if ( not os.path.exists(self.model_save_path) ):
+            if(not os.path.exists(os.path.join('model',self.model))):
+                os.mkdir(os.path.join('model',self.model))
             os.mkdir(self.model_save_path)
 
         self.generate_image_path = os.path.join('data',self.model,self.data_name + '_generate_image')
